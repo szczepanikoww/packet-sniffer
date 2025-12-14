d0955eaf635d055a79f1b487d005886b587a1aa6
Michał Szczepański <mszczepanski@student.agh.edu.pl>
2025-12-14 23:36:46 +0100

    capturing ethernet frames

---
+++ main.py
@@ -0,0 +1,26 @@
+import socket
+import struct
+import textwrap
+
+# keep looping to receive packets
+def main():
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+    while True:
+        raw_data, addr = conn.recvfrom(65536)
+        dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
+        print('\nEthernet Frame:')
+        print('Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
+
+#unpack ethernet frame
+def ethernet_frame(data):
+    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
+    return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
+
+#return propelrly formatted MAC address (11:11:11:11:11:11)
+def get_mac_addr(bytes_addr):
+    bytes_str = map('{:02x}'.format, bytes_addr)
+    mac_addr = ':'.join(bytes_str).upper()
+    return mac_addr
+
+main()
